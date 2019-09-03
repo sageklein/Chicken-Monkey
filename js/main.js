7551e8d054e5de7e59c7a813628c3353e3aa6a8f
Sage Klein <sageklein@gmail.com>
2019-09-03 11:20:08 -0500

    added main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) 
+{
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+    console.log("Chicken Monkey");
+    }
+    else if (currentNumber % 5 === 0 ) {
+    console.log("Chicken");
+    }
+    else if (currentNumber % 7 === 0 ) {
+    console.log("Monkey"); 
+    }
+    else
+    {
+    console.log(currentNumber) ;
+    }
+        
+}
